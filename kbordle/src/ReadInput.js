b8233e5d421376d8a704f7c33e721135c8cb6ada
정현규 <gusrbwjd1229@gmail.com>
2023-04-27 00:47:03 +0900

    입력 창 생성

---
+++ kbordle/src/ReadInput.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+class ReadInput extends React.Component {
+    render() {
+        return (
+            <div>
+                <input />
+                <button>입력</button>
+            </div>
+        )
+    }
+}
+
+export default ReadInput;
